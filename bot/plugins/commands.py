74e3102a18e02eaaec0ce6203d1a296441b68221
Dev on java <95332799+JON-WICK-TG@users.noreply.github.com>
2021-12-15 21:22:37 +0530

    Try

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -123,7 +123,7 @@ async def start(bot, update):
         InlineKeyboardButton('Channel 📜', url=f'{MT_CHANNEL}'),
     ],[
         InlineKeyboardButton('⚙️ Help', callback_data="help")
-        InlineKeyboardButton('Close 🔒', url=f'{MT_CHANNEL}')
+        InlineKeyboardButton('Close', callback_data='close')
     ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
