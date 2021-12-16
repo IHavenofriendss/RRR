8b56329d3354014208632f44743bd1779cd21981
BLACK DEVIL TG <95171491+BLACK-DEVIL-TG@users.noreply.github.com>
2021-12-16 06:58:05 +0530

    Help Remove

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -138,18 +138,6 @@ async def start(bot, update):
     )
 
 
-@Client.on_message(filters.command(["help"]) & filters.private, group=1)
-async def help(bot, update):
-    
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.HELP_TEXT,
-        reply_markup=reply_markup,
-        parse_mode="html",
-        reply_to_message_id=update.message_id
-    )
-
-
 @Client.on_message(filters.command(["about"]) & filters.private, group=1)
 async def about(bot, update):
     
